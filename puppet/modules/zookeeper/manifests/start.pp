e2b7d90e57f8ecee34548316dc5d0832638db43d
Cosmin Cătălin SANDA <cosmincatalin@gmail.com>
2014-12-04 13:59:45 +0100

    Update start.pp

--- puppet/modules/zookeeper/manifests/start.pp
+++ puppet/modules/zookeeper/manifests/start.pp
@@ -1,14 +1,6 @@
 # Starts a Zookeeper server
 class zookeeper::start($user) {
 
-  $hadoopHome = "/home/${user}/hadoop"
-  $hadoopSbin = "${hadoopHome}/sbin"
-  $hadoopBin = "${hadoopHome}/bin"
-  $hadoopConf = "${hadoopHome}/etc/hadoop"
-  $hadoopDaemon = "${hadoopSbin}/hadoop-daemon.sh --config ${hadoopConf}"
-  $yarnDaemon = "${hadoopSbin}/yarn-daemon.sh --config ${hadoopConf}"
-  $jhDaemon = "${hadoopSbin}/mr-jobhistory-daemon.sh --config ${hadoopConf}"
-
   Exec {
     path  => '/bin:/usr/bin:/sbin',
     user  => $user
